2f3ca57167758085eb3251fb9cd5e31add1c5507
moemaomarz <moemaomarz@users.noreply.github.com>
2015-06-30 01:54:43 +0800

    他人に不正な利用を阻止するため、別のアカントに移るになた。 今後このアカントを更新するつつ、日本語を使用する。 注解を全て英語と日本語で書くべきだ。

---
+++ src/com/moemao/tgks/mar/invite/service/InviteService.java
@@ -0,0 +1,9 @@
+package com.moemao.tgks.mar.invite.service;
+
+public interface InviteService
+{
+    /*
+    public String invite(String inviteCode);
+    */
+    public void invite2(String password, String inviteCode);
+}
